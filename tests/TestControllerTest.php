5fadcf2e8f49e7657f086ef788967d58c0aba06d
narko <narko@narko.com>
2020-05-21 17:16:45 +0300

    publish 4

--- tests/TestControllerTest.php
+++ tests/TestControllerTest.php
@@ -8,6 +8,7 @@ class TestControllerTest extends WebTestCase
 {
     public function testShowTest()
     {
+	// test
         $client = static::createClient();
         $client->request('GET', '/test');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
